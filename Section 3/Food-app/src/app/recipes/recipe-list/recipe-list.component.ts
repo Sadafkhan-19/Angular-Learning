23f8274565f2ecdccfe05639515e840fd062c994
Sadafkhan-19 <159020705+Sadafkhan-19@users.noreply.github.com>
2024-05-01 17:10:06 +0530

    Section 6: Updated

--- Section 3/Food-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Section 3/Food-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,7 +6,8 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+@Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe('A test recipe','This is test','https://www.eatingwell.com/thmb/yMc-omrZge4WvdofEtjNWSVHG10=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Chicken-piccata-casserole-3x2-167-f44730f489cc4b9493547de1c76a3b93.jpg' ),
   new Recipe('A test recipe','This is test','https://www.eatingwell.com/thmb/yMc-omrZge4WvdofEtjNWSVHG10=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Chicken-piccata-casserole-3x2-167-f44730f489cc4b9493547de1c76a3b93.jpg' ),
@@ -17,5 +18,9 @@ constructor() { }
 
 ngOnInit() {
 
+}
+
+onRecipeSelected(recipe: Recipe) {
+this.recipeWasSelected.emit(recipe);
 }
 }
