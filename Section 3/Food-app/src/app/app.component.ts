e3853445ca6d77cd7c3aa798dc98813aa98e9bc5
Sadafkhan-19 <159020705+Sadafkhan-19@users.noreply.github.com>
2024-05-03 13:41:10 +0530

    Section 8: Uodated in section 3

--- Section 3/Food-app/src/app/app.component.ts
+++ Section 3/Food-app/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Food-app';
-  loadedFeature = 'recipe'
+  loadedFeature = 'recipe';
   onNavigate(feature: string) {
 this.loadedFeature = feature;
   }
