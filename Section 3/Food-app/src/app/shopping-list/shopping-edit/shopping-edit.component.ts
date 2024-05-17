5b82ec19ce9f0340dbd7d4f047206107f80515f0
Sadafkhan-19 <159020705+Sadafkhan-19@users.noreply.github.com>
2024-05-17 16:25:49 +0530

    Updated

--- Section 3/Food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Section 3/Food-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.css'
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+@ViewChild('nameInput') nameInputRef: ElementRef;
+@ViewChild('amountInput') amountInputRef: ElementRef;
 
+constructor(private alService: ShoppingListService) {}
+  ngOnInit() {
+   
+  }
+onAddItem () {
+  const ingName = this.nameInputRef.nativeElement.value;
+  const ingAmount = this.nameInputRef.nativeElement.value;
+  const newIngredient = this.nameInputRef.nativeElement.value;
+  this.alService.addIngredeinet(newIngredient);
+}
 }
--- Section 3/Food-app/src/app/shopping-list/shopping-list.service.ts
+++ Section 3/Food-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Mango', 14)
+      ];
+
+      getIngredients() {
+        return this.ingredients.slice();
+      }
+
+      addIngredeinet(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+      }
 }
